d9619d6098dfde870eac1d793ba50956575ac1c4
Aileen Tran <aileen.tran27@gmail.com>
2019-11-05 17:13:50 -0800

    Fleshed out server.py, installed flask_debugtoolbar, updated requirements.txt, created base.html and homepage.html

--- server.py
+++ server.py
@@ -1,8 +1,27 @@
-from flask import Flask
-from model import connect_to_db
+from jinja2 import StrictUndefined
+
+from flask import (Flask, render_template, redirect, request, flash, session)
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import db, connect_to_db, User, Route, Segment, Mode 
 
 
 app = Flask(__name__)
+# required to have secret key to use Flask sessions and debug toolbar
+# TODO: CHANGE SECRET KEY!! 
+app.secret_key='SUPER SECRET'
+
+# Throws up error if have undefined error in Jinja2
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage: map, search bar, and register/login buttons"""
+
+    return render_template('homepage.html')
+
 
 
 if __name__ == "__main__":
