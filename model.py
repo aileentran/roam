be3e6c7d7794cb4374470e2e220478209909a4ab
Aileen Tran <aileen.tran27@gmail.com>
2019-11-06 13:02:00 -0800

    Changed password on registration.html to dots, hashed/encrypted passwords on model.py, and updated requirements.txt

--- model.py
+++ model.py
@@ -3,6 +3,8 @@
 
 from flask_sqlalchemy import SQLAlchemy 
 
+# allows us to hash/encrypt passwords
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -18,7 +20,7 @@ class User(db.Model):
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     email = db.Column(db.String(75), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     # String based on Twilio's messaging API - '+1234567890'
     phone = db.Column(db.String(15))
 
@@ -29,6 +31,13 @@ class User(db.Model):
         """Show user information when return object!"""
 
         return f"<User user_id={self.user_id} email={self.email} phone={self.phone}>"
+
+    #setting functions to encrypt/hash passwords
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 # Routes table
