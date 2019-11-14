e879532e7e98465dc24691476779b03f4b2488cd
Aileen Tran <aileen.tran27@gmail.com>
2019-11-13 17:46:23 -0800

    Added gmaps result in comment

--- server.py
+++ server.py
@@ -133,6 +133,10 @@ def save_route():
 	# this is a LIST of info!! 
 	start_info = gmaps.geocode(start_address)
 
+	#result: 
+	# [{'address_components': [{'long_name': '1600', 'short_name': '1600', 'types': ['street_number']}, {'long_name': 'Amphitheatre Parkway', 'short_name': 'Amphitheatre Pkwy', 'types': ['route']}, {'long_name': 'Mountain View', 'short_name': 'Mountain View', 'types': ['locality', 'political']}, {'long_name': 'Santa Clara County', 'short_name': 'Santa Clara County', 'types': ['administrative_area_level_2', 'political']}, {'long_name': 'California', 'short_name': 'CA', 'types': ['administrative_area_level_1', 'political']}, {'long_name': 'United States', 'short_name': 'US', 'types': ['country', 'political']}, {'long_name': '94043', 'short_name': '94043', 'types': ['postal_code']}], 'formatted_address': '1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA', 'geometry': {'location': {'lat': 37.4215785, 'lng': -122.0837816}, 'location_type': 'ROOFTOP', 'viewport': {'northeast': {'lat': 37.42292748029149, 'lng': -122.0824326197085}, 'southwest': {'lat': 37.4202295197085, 'lng': -122.0851305802915}}}, 'place_id': 'ChIJtYuu0V25j4ARwu5e4wwRYgE', 'plus_code': {'compound_code': 'CWC8+JF Mountain View, California, United States', 'global_code': '849VCWC8+JF'}, 'types': ['street_address']}]
+
+
 	for info in start_info:
 		if 'geometry' in info:
 			print('hi')
