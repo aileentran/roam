be53d26827e0986e15761a718049ed762f9c2f47
Aileen Tran <aileen.tran27@gmail.com>
2019-11-16 17:09:36 -0800

    Removed current location for now. Linked routes back to map page

--- static/js/map_base.js
+++ static/js/map_base.js
@@ -1,9 +1,11 @@
-// What does this do? 
+// JS will show syntax errors! 
 "use strict";
 
 // this function called in homepage.html inside API connection
-function initMap(){
-	// Returns Geolocation object, gives Web content access to location of devide = get user's current location! 
+function initMap(){ 
+
+	const infoWindow = new google.maps.InfoWindow;
+
 	const map = new google.maps.Map(
 	document.getElementById("map"),
 	{
@@ -11,26 +13,5 @@ function initMap(){
 		center: {lat: 37.601773, lng: -122.202870},
 		zoom: 11
 	})
-	const infoWindow = new google.maps.InfoWindow;
-
-	// if allowed/able to get device's location
-	if (navigator.geolocation){
-		// getting device's position
-		navigator.geolocation.getCurrentPosition((position) => {
-			const pos = {
-				lat: position.coords.latitude,
-				lng: position.coords.longitude
-			};
-
-			infoWindow.setPosition(pos);
-			infoWindow.setContent("Current location");
-			infoWindow.open(map);
-			map.setCenter(pos);
-		}
-			);
-	} else {
-		// not allowed or not able to access device's location
-		return "We cannot access your current location. Please input your address." 
-	}
 
 };
